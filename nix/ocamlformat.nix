db9a8736916a4d68a3794a5a351ed87dc1518417
Nymphium <s1311350@gmail.com>
2025-01-19 03:57:39 +0900

    Run format

--- nix/ocamlformat.nix
+++ nix/ocamlformat.nix
@@ -1,13 +1,14 @@
-{ pkgs, lib, ocamlformat }:
+{
+  pkgs,
+  lib,
+  ocamlformat,
+}:
 let
   ocamlformat_config = lib.strings.splitString "\n" (builtins.readFile ocamlformat);
   re = builtins.match "version\s*=\s*(.*)\s*$";
-  version_line = lib.lists.findFirst
-    (l: builtins.isList (re l))
-    (throw "no version specified in .ocamlformat")
-    ocamlformat_config;
+  version_line = lib.lists.findFirst (
+    l: builtins.isList (re l)
+  ) (throw "no version specified in .ocamlformat") ocamlformat_config;
   version = builtins.elemAt (re version_line) 0;
 in
-builtins.trace
-  "detect ocamlformat version: ${version}"
-  version
+builtins.trace "detect ocamlformat version: ${version}" version
